2a2b1bc186d067c831e3fe49a2374c3ad6cbb9bc
Diego Canuhé <diego@dcanuhe.com>
2023-05-21 20:31:38 -0300

    wrap event handler inside useCallback()

--- secret-files-client/src/components/FileNameFilter.js
+++ secret-files-client/src/components/FileNameFilter.js
@@ -1,14 +1,14 @@
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
-import {useState} from 'react'
+import {useCallback, useState} from 'react'
 
 export default function FileNameFilter({onFilter, className}) {
   const [nameFilter, setNameFilter] = useState('')
 
-  const handleSubmit = ev => {
+  const handleSubmit = useCallback(ev => {
     ev.preventDefault()
     onFilter && onFilter(nameFilter)
-  }
+  }, [onFilter, nameFilter])
 
   return <Form onSubmit={handleSubmit} className={className}>
     <Form.Control placeholder="Filter by name" onChange={ev => setNameFilter(ev.target.value)} />
