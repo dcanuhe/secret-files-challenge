c43d195949af339941b3c1f003fb732e4afbc2e6
Diego Canuhé <diego@dcanuhe.com>
2023-05-21 20:56:16 -0300

    clear error after a successful request

--- secret-files-client/src/filesData.js
+++ secret-files-client/src/filesData.js
@@ -14,6 +14,7 @@ export function useFilesData() {
       .then(data => {
         setData(data)
         setLoading(false)
+        setError(null)
       })
       .catch(error => {
         setError(error)
